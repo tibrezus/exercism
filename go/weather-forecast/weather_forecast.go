edb577b22a929bfd3fcd4b0828f23ecf28d433e3
Tiberiu Rezus <tiberiu.rezus@gmail.com>
2024-06-21 23:07:40 +0200

    Refactor weather forecast code structure

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,10 +1,13 @@
 // Package weather provides a weather forecast for a given city.
 package weather
 
-// CurrentCondition is the current weather condition for the city.
-var CurrentCondition string
-// CurrentLocation is the city for which the weather is being forecasted.
-var CurrentLocation string
+
+var (
+	// CurrentCondition is the current weather condition for the city.
+	CurrentCondition string
+	// CurrentLocation is the city for which the weather is being forecasted.
+	CurrentLocation string
+)
 
 // Forecast returns the current weather condition for a given city.
 func Forecast(city, condition string) string {
