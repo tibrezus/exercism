5f2505bc7c799889a68b4521d532cba2bab0da34
Tiberiu Rezus <tiberiu.rezus@gmail.com>
2024-06-22 20:38:36 +0200

    Car Assemble

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,34 @@
+// Package cars implements a solution to the cars-assemble exercise
+package cars
+
+import "errors"
+
+// verifySuccessRate calculates if the successRate is within the
+// acceptable range.
+func verifySuccessRate(successRate float64) error {
+	if successRate < 0 || successRate > 100 {
+		return errors.New("successRate must be between 0 and 100")
+	}
+	return nil
+}
+
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate / 100
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	group := carsCount / 10
+	individual := carsCount % 10
+
+	return uint(group * 95000 + individual * 10000)
+}
